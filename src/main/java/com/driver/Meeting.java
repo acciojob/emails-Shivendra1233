397c94687e504612ef7d57f5ee7909925f4eb957
Shivendra1233 <132485033+Shivendra1233@users.noreply.github.com>
2023-11-25 05:41:57 -0800

    Meeting.java
    Attempt 1

--- src/main/java/com/driver/Meeting.java
+++ src/main/java/com/driver/Meeting.java
@@ -3,12 +3,20 @@ package com.driver;
 import java.time.LocalTime;
 
 public class Meeting {
-    private LocalTime startTime;
-    private LocalTime endTime;
+
+    private final LocalTime startTime;
+    private final LocalTime endTime;
 
     public Meeting(LocalTime startTime, LocalTime endTime){
         this.startTime = startTime;
         this.endTime = endTime;
     }
 
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
 }
